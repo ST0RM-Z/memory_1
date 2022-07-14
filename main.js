9dc4d3bb165ccc7c849271b0b936013061b053da
Dhairya arya <dhairya@Dhairyas-MacBook-Air.local>
2022-07-13 23:30:35 -0400

    randomImage

--- main.js
+++ main.js
@@ -1,64 +1,48 @@
-"use strict";
+// "use strict";;
 
-$( function(){
-    $( "#tabs" ).tabs();
-   } );
+$(document).ready(function () {
+  $("#tabs").tabs();
+  var data = randomImg.randomNum(24);
 
-   var Card_Img = [
-    "images/card_1.png",
-    "images/card_2.png",
-    "images/card_3.png",
-    "images/card_4.png",
-    "images/card_5.png",
-    "images/card_6.png",
-    "images/card_7.png",
-    "images/card_8.png",
-    "images/card_9.png",
-    "images/card_10.png",
-    "images/card_11.png",
-    "images/card_12.png",
-    "images/card_13.png",
-    "images/card_14.png",
-    "images/card_15.png",
-    "images/card_16.png",
-    "images/card_17.png",
-    "images/card_18.png",
-    "images/card_19.png",
-    "images/card_20.png",
-    "images/card_21.png",
-    "images/card_22.png",
-    "images/card_23.png",
-    "images/card_24.png"];
-   
-
-
-   function nextImage(elecardsment)
- {
-//    var img = document.getElementById(element);
-      document.getElementById(cards).src = imgArray[0].src;
- }
-
-   $(document).ready( () => {
-
-    if(sessionStorage.getItem("player_name") != null){
-        $("#player").text('Player: '+sessionStorage.getItem("player_name"));
-
-
+  console.log(data);
+  // console.log("a");
+  // ----------------------calling function in document.ready method
+  nextImage();
+  function nextImage() {
+    var cards = document.getElementById("cards");
+    let rows = 4;
+    let col = 4;
+    console.log("src");
+    for (var i = 0; i < rows; i++) {
+      // console.log("in--outer--loop");
+      var tr = document.createElement("tr");
+      for (var j = 0; j < col; j++) {
+        // console.log("in--inner--loop", Card_Img[0]);
+        // var src = Card_Img[0];
+        var td = document.createElement("td");
+        const img = document.createElement("img");
+        var data = randomImg.randomNum(24);
+        // console.log(data);
+        img.setAttribute("src", Card_Img[data]);
+        // console.log(img);
+        td.appendChild(img);
+        tr.appendChild(td);
+      }
+      cards.appendChild(tr);
     }
-              $( "#save_settings" ).click( () => {    
-                
-                
-                const player_name = $("#player_name").val();
-                const num_cards = $("#num_cards").val();
-                
-                    sessionStorage.setItem("player_name", player_name);
-                    sessionStorage.setItem("num_cards", num_cards);
-                    location.reload();
-                
-                
-            });
+  }
+});
 
-         
+$(document).ready(() => {
+  if (sessionStorage.getItem("player_name") != null) {
+    $("#player").text("Player: " + sessionStorage.getItem("player_name"));
+  }
+  $("#save_settings").click(() => {
+    const player_name = $("#player_name").val();
+    const num_cards = $("#num_cards").val();
 
-  
-}); 
+    sessionStorage.setItem("player_name", player_name);
+    sessionStorage.setItem("num_cards", num_cards);
+    location.reload();
+  });
+});
---
+++ random.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// OBJECT LITERAL DECLARE
+const randomImg = {
+  randomNum(max) {
+    return Math.floor(Math.random() * max);
+  },
+};
