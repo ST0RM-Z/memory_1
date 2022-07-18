088aa88b22b26ec0517436d48ec05ba4b8c86f5f
Dhairya arya <dhairya@Dhairyas-MacBook-Air.local>
2022-07-18 18:58:59 -0400

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,17 +16,3 @@ $(document).ready(function () {
     store_session();
   }
 });
-
-// $(document).ready(() => {
-//   if (sessionStorage.getItem("player_name") != null) {
-//     $("#player").text("Player: " + sessionStorage.getItem("player_name"));
-//   }
-//   $("#save_settings").click(() => {
-//     const player_name = $("#player_name").val();
-//     const num_cards = $("#num_cards").val();
-
-//     sessionStorage.setItem("player_name", player_name);
-//     sessionStorage.setItem("num_cards", num_cards);
-//     location.reload();
-//   });
-// });
