4dbefc289e0eed50b0e3c940bc00cf8e1a4f5960
Dhairya arya <dhairya@Dhairyas-MacBook-Air.local>
2022-07-29 14:25:25 -0400

    flag

--- helper/imageLogic.js
+++ helper/imageLogic.js
@@ -2,6 +2,7 @@ function imageLogic() {
   var cards = document.getElementById("cards");
   let rows = 4;
   let col = 4;
+  var flag = false;
   console.log("src");
   for (var i = 0; i < rows; i++) {
     // console.log("in--outer--loop");
@@ -12,12 +13,23 @@ function imageLogic() {
       var td = document.createElement("td");
       const img = document.createElement("img");
       var data = randomImg.randomNum(24);
+
+      if (!flag) {
+        img.setAttribute("src", "../images/back.png");
+        img.setAttribute("id", i);
+      } else {
+        //   img.setAttribute("src", "../images/back.png")
+        img.setAttribute("src", Card_Img[data]);
+      }
       // console.log(data);
-      img.setAttribute("src", Card_Img[data]);
       // console.log(img);
       td.appendChild(img);
       tr.appendChild(td);
     }
     cards.appendChild(tr);
+    $(`#${i}`).click(function () {
+      console.log("imgid", i);
+      flag = true;
+    });
   }
 }
